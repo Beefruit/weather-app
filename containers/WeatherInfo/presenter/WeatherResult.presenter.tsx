e6d327e04a3763edce6dbd337e26b36b0ee98ed8
juaovo <a01068020264@gmail.com>
2025-11-12 21:47:53 +0900

    result css

--- containers/WeatherInfo/presenter/WeatherResult.presenter.tsx
+++ containers/WeatherInfo/presenter/WeatherResult.presenter.tsx
@@ -13,23 +13,23 @@ const WeatherResultPresenter: FC = () => {
         <h2 className={cx("temperature")}>22&deg;C</h2>
         <p className={cx("weather")}>맑음</p>
       </div>
-      <div className={cx("weather-information")}>
-        <div className={cx("hummidity")}>
-          <Droplets size={24} className={cx("hummidity-icon")} />
+      <ul className={cx("weather-information")}>
+        <li className={cx("weather-item")}>
+          <Droplets size={24} className={cx("humidity-icon")} />
           <h2 className={cx("weather-title")}>습도</h2>
           <p className={cx("weather-description")}>45%</p>
-        </div>
-        <div className={cx("wind")}>
+        </li>
+        <li className={cx("weather-item")}>
           <Wind size={24} className={cx("wind-icon")} />
           <h2 className={cx("weather-title")}>풍속</h2>
           <p className={cx("weather-description")}>8Km/h</p>
-        </div>
-        <div className={cx("wind-chill")}>
+        </li>
+        <li className={cx("weather-item")}>
           <Thermometer size={24} className={cx("wind-chill-icon")} />
           <h2 className={cx("weather-title")}>체감온도</h2>
           <p className={cx("weather-description")}>24&deg;C</p>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 };
