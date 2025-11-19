d7e9ddf0035a016acc22e8d8fe226f1a694b5b94
이형진 <hyeolee@zempot.com>
2025-11-19 20:07:00 +0900

    test

--- containers/WeatherInfo/hook/useWeatherInfo.ts
+++ containers/WeatherInfo/hook/useWeatherInfo.ts
@@ -1,11 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSearchStore } from "@/store/searchQuery.store";
-import { getGeocodeApi } from "../api/weatherInfo.api";
+import { getGeocodeApi, getWeatherApi } from "../api/weatherInfo.api";
 
 export const useWeatherInfo = () => {
   const { searchQuery } = useSearchStore();
+  const [weatherInfo, setWeatherInfo] = useState<{
+    temperature: number;
+    weather: string;
+    humidity: number;
+    windSpeed: number;
+    feelsLike: number;
+    iconUrl: string;
+  } | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -15,11 +23,21 @@ export const useWeatherInfo = () => {
 
       const data = await getGeocodeApi(searchQuery);
 
-      console.log(data);
+      const weatherData = await getWeatherApi({ lat: data.lat, lon: data.lng });
+
+      setWeatherInfo({
+        temperature: weatherData.daily[0].temp.eve,
+        weather: weatherData.daily[0].weather[0].main,
+        humidity: weatherData.daily[0].humidity,
+        windSpeed: weatherData.daily[0].wind_speed,
+        feelsLike: weatherData.daily[0].feels_like.eve,
+        iconUrl: `http://openweathermap.org/img/wn/${weatherData.daily[0].weather[0].icon}.png`,
+      });
     })();
   }, [searchQuery]);
 
   return {
     searchQuery,
+    weatherInfo,
   };
 };
--- containers/WeatherInfo/presenter/WeatherResult.presenter.tsx
+++ containers/WeatherInfo/presenter/WeatherResult.presenter.tsx
@@ -7,33 +7,47 @@ const cx = classNames.bind(styles);
 
 interface IWeatherResultPresenterProps {
   searchQuery: string;
+  weatherInfo: {
+    temperature: number;
+    weather: string;
+    humidity: number;
+    windSpeed: number;
+    feelsLike: number;
+    iconUrl: string;
+  } | null;
 }
 
 const WeatherResultPresenter: FC<IWeatherResultPresenterProps> = ({
   searchQuery,
+  weatherInfo,
 }) => {
   return (
     <div className={cx("home-result")}>
       <div className={cx("location")}>
         <h2 className={cx("city")}>{searchQuery}</h2>
-        <h2 className={cx("temperature")}>22&deg;C</h2>
-        <p className={cx("weather")}>맑음</p>
+        <img src={weatherInfo?.iconUrl} alt={weatherInfo?.weather} />
+        <h2 className={cx("temperature")}>{weatherInfo?.temperature}&deg;C</h2>
+        <p className={cx("weather")}>{weatherInfo?.weather}</p>
       </div>
       <ul className={cx("weather-information")}>
         <li className={cx("weather-item")}>
           <Droplets size={24} className={cx("humidity-icon")} />
           <h2 className={cx("weather-title")}>습도</h2>
-          <p className={cx("weather-description")}>45%</p>
+          <p className={cx("weather-description")}>{weatherInfo?.humidity}%</p>
         </li>
         <li className={cx("weather-item")}>
           <Wind size={24} className={cx("wind-icon")} />
           <h2 className={cx("weather-title")}>풍속</h2>
-          <p className={cx("weather-description")}>8Km/h</p>
+          <p className={cx("weather-description")}>
+            {weatherInfo?.windSpeed}Km/h
+          </p>
         </li>
         <li className={cx("weather-item")}>
           <Thermometer size={24} className={cx("wind-chill-icon")} />
           <h2 className={cx("weather-title")}>체감온도</h2>
-          <p className={cx("weather-description")}>24&deg;C</p>
+          <p className={cx("weather-description")}>
+            {weatherInfo?.feelsLike}&deg;C
+          </p>
         </li>
       </ul>
     </div>
